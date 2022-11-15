0f9ead49a19c19cfc21038da23d3fbb723239e95
wasu <61418403+wasu-code@users.noreply.github.com>
2022-11-15 03:44:30 +0100

    filtry, ochrona dostępu do zasobów

---
+++ CreditCalc/src/main/java/com/jsfcourse/login/LoginBB.java
@@ -0,0 +1,88 @@
+package com.jsfcourse.login;
+
+
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.simplesecurity.RemoteClient;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.jsf.entities.Person;
+
+
+@Named
+@RequestScoped
+public class LoginBB {
+	private static final String PAGE_MAIN = "/index?faces-redirect=true";
+	private static final String PAGE_LOGIN = "/login";
+	private static final String PAGE_STAY_AT_THE_SAME = null;
+
+	private String login;
+	private String pass;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+
+	public String doLogin() {
+		FacesContext ctx = FacesContext.getCurrentInstance();
+
+		// 1. verify login and password - get User from "database"
+		Person user = new Person();
+		user.setName("Jan");
+		user.setSurname("Fajny");
+
+		// 2. if bad login or password - stay with error info
+		if (user == null) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					"Niepoprawny login lub hasło", null));
+			return PAGE_STAY_AT_THE_SAME;
+		}
+
+		// 3. if logged in: get User roles, save in RemoteClient and store it in session
+		
+		RemoteClient<Person> client = new RemoteClient<Person>(); //create new RemoteClient
+		client.setDetails(user);
+		
+		
+		client.getRoles().add("user");
+		
+	
+		//store RemoteClient with request info in session (needed for SecurityFilter)
+		HttpServletRequest request = (HttpServletRequest) ctx.getExternalContext().getRequest();
+		client.store(request);
+
+		// and enter the system (now SecurityFilter will pass the request)
+		return PAGE_MAIN;
+	}
+	
+	public String doLogout(){
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(true);
+		//Invalidate session
+		// - all objects within session will be destroyed
+		// - new session will be created (with new ID)
+		session.invalidate();
+		return PAGE_LOGIN;
+	}
+	
+}
